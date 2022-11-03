421a03996182dd14302d476be6593125ec93f285
JPaja <jovan.pavlovic99@gmail.com>
2022-11-03 20:30:51 +0100

    Add: Pattern base

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,61 @@
+use std::{ops::RangeInclusive, result};
+
 mod rangeutils;
+
+pub struct BruteIterator {
+    pattern: Pattern,
+}
+
+pub enum Pattern {
+    Range(RangeInclusive<char>),
+    Ranges(Vec<RangeInclusive<char>>),
+    Repeater(Box<Pattern>, RangeInclusive<u32>),
+    Patterns(Vec<Box<Pattern>>),
+}
+
+
+impl IntoIterator for BruteIterator
+{
+    type Item = String;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        todo!()
+    }
+}
+
+impl Pattern {
+    fn size(&self) -> Option<u128> {
+        match &self {
+            Self::Range(range) => rangeutils::range_size(range),
+            Self::Ranges(ranges) => ranges.iter().fold(Some(0u128), |buf, r| {
+                buf?.checked_mul(rangeutils::range_size(r)?)
+            }),
+            Self::Repeater(pattern, range) => {
+                let len = pattern.size()?;
+                if range.is_empty() {
+                    return Some(0);
+                }
+                let combo = len.checked_pow(*range.start())?;
+                Some(
+                    RangeInclusive::new(*range.start() + 1, *range.end())
+                        .into_iter()
+                        .fold(Some((combo, combo)), |buf, _| {
+                            let (total_size, last_size) = buf?;
+                            let size = last_size.checked_mul(len)?;
+                            Some((total_size.checked_mul(size)?, size))
+                        })?
+                        .0,
+                )
+            }
+            Self::Patterns(patterns) => patterns.iter().fold(Some(1u128), |buf, p| {
+                let size = p.as_ref().size()?;
+                if size == 0 {
+                    buf
+                } else {
+                    buf?.checked_mul(size)
+                }
+            }),
+        }
+    }
+}
--- src/rangeutils.rs
+++ src/rangeutils.rs
@@ -12,76 +12,50 @@ fn gen_ranges(pattern: &str) -> Vec<RangeInclusive<char>> {
     merge_and_order_ranges(ranges)
 }
 
-fn range_lowercase_letters() -> RangeInclusive<char> {
-    'a'..='z'
-}
+const RANGE_LOWERCASE_LETTERS: RangeInclusive<char> = 'a'..='z';
 
-fn range_uppercase_letters() -> RangeInclusive<char> {
-    'A'..='Z'
-}
+const RANGE_UPPERCASE_LETTERS: RangeInclusive<char> = 'A'..='Z';
 
-fn range_letters() -> Vec<RangeInclusive<char>> {
-    vec![range_uppercase_letters(), range_lowercase_letters()]
-}
+const RANGE_LETTERS: [RangeInclusive<char>; 2] = [RANGE_UPPERCASE_LETTERS, RANGE_LOWERCASE_LETTERS];
 
-fn merge_and_order_ranges_vec(ranges: Vec<RangeInclusive<char>>) -> Vec<RangeInclusive<char>> {
-    merge_and_order_ranges(ranges.into_iter())
-}
+const RANGE_NUMBERS: RangeInclusive<char> = '0'..='9';
 
-fn range_numbers() -> RangeInclusive<char> {
-    '0'..='9'
-}
+const RANGE_HEX_LOWERCASE: [RangeInclusive<char>; 2] = [RANGE_NUMBERS, 'a'..='f'];
 
-fn range_hex_lowercase() -> Vec<RangeInclusive<char>> {
-    vec![range_numbers(), 'a'..='f']
-}
+const RANGE_HEX_UPPERCASE: [RangeInclusive<char>; 2] = [RANGE_NUMBERS, 'A'..='F'];
 
-fn range_hex_uppercase() -> Vec<RangeInclusive<char>> {
-    vec![range_numbers(), 'A'..='F']
-}
+const RANGE_HEX: [RangeInclusive<char>; 3] = [RANGE_NUMBERS, 'A'..='F', 'a'..='f'];
 
-fn range_hex() -> Vec<RangeInclusive<char>> {
-    vec![range_numbers(), 'A'..='F', 'a'..='f']
-}
+const RANGE_ALPHANUM_LOWERCASE: [RangeInclusive<char>; 2] =
+    [RANGE_NUMBERS, RANGE_LOWERCASE_LETTERS];
 
-fn range_alphanum_lowercase() -> Vec<RangeInclusive<char>> {
-    vec![range_numbers(), range_lowercase_letters()]
-}
+const RANGE_ALPHANUM_UPPERCASE: [RangeInclusive<char>; 2] =
+    [RANGE_NUMBERS, RANGE_UPPERCASE_LETTERS];
 
-fn range_alphanum_uppercase() -> Vec<RangeInclusive<char>> {
-    vec![range_numbers(), range_uppercase_letters()]
-}
+const RANGE_ALPHANUM: [RangeInclusive<char>; 3] = [
+    RANGE_NUMBERS,
+    RANGE_UPPERCASE_LETTERS,
+    RANGE_LOWERCASE_LETTERS,
+];
 
-fn range_alphanum() -> Vec<RangeInclusive<char>> {
-    vec![
-        range_numbers(),
-        range_uppercase_letters(),
-        range_lowercase_letters(),
-    ]
-}
+const RANGE_PUNCT: [RangeInclusive<char>; 4] = ['!'..='/', ':'..='@', '['..='`', '{'..='~'];
 
-fn range_punct() -> Vec<RangeInclusive<char>> {
-    range!("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
-}
-
-fn range_flatten_vec(ranges: Vec<RangeInclusive<char>>) -> impl Iterator<Item = char> {
-    range_flatten(ranges.into_iter())
-}
-
-fn range_flatten<I>(ranges: I) -> impl Iterator<Item = char>
+fn ranges_flatten<I, T>(ranges: I) -> T
 where
-    I: Iterator<Item = RangeInclusive<char>>,
+    I: IntoIterator<Item = RangeInclusive<char>>,
+    T: FromIterator<char>,
 {
-    let sorted = merge_and_order_ranges(ranges);
-    let res = sorted.into_iter().flatten();
-    res.into_iter()
+    let merged: Vec<RangeInclusive<char>> = merge_and_order_ranges(ranges);
+    merged.into_iter().flatten().collect()
 }
 
-fn merge_and_order_ranges<I>(ranges: I) -> Vec<RangeInclusive<char>>
+fn merge_and_order_ranges<I, T>(ranges: I) -> T
 where
-    I: Iterator<Item = RangeInclusive<char>>,
+    I: IntoIterator<Item = RangeInclusive<char>>,
+    T: FromIterator<RangeInclusive<char>>,
 {
     let mut sorted = ranges
+        .into_iter()
         .filter(|r| !r.is_empty())
         .sorted_by(|r1, r2| r1.start().cmp(r2.start()));
     let mut result: Vec<RangeInclusive<char>> = vec![];
@@ -102,7 +76,14 @@ where
         }
         result.push(range);
     }
-    result
+    result.into_iter().collect()
+}
+
+pub fn range_size(range: &RangeInclusive<char>) -> Option<u128> {
+    match range.size_hint().1?.try_into() {
+        Ok(size) => Some(size),
+        _ => None,
+    }
 }
 
 #[cfg(test)]
@@ -111,163 +92,173 @@ mod tests {
 
     #[test]
     fn test_merge_ranges_emtpy() {
-        let result = merge_and_order_ranges_vec(vec![]);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges([]);
         assert_eq!(result, vec![]);
     }
 
     #[test]
     fn test_merge_ranges_single() {
-        let result = merge_and_order_ranges_vec(vec!['a'..='z']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['a'..='z']);
         assert_eq!(result, vec!['a'..='z']);
     }
 
     #[test]
     fn test_merge_ranges_multi() {
-        let result = merge_and_order_ranges_vec(vec!['0'..='9', 'A'..='Z', 'a'..='z']);
+        let result: Vec<RangeInclusive<char>> =
+            merge_and_order_ranges(['0'..='9', 'A'..='Z', 'a'..='z']);
         assert_eq!(result, vec!['0'..='9', 'A'..='Z', 'a'..='z']);
     }
 
     #[test]
     fn test_merge_ranges_multi_sort() {
-        let result = merge_and_order_ranges_vec(vec!['a'..='z', 'A'..='Z', '0'..='9']);
-        assert_eq!(result, vec!['0'..='9', 'A'..='Z', 'a'..='z',]);
+        let result: Vec<RangeInclusive<char>> =
+            merge_and_order_ranges(['a'..='z', 'A'..='Z', '0'..='9']);
+        assert_eq!(result, vec!['0'..='9', 'A'..='Z', 'a'..='z']);
     }
 
     #[test]
     fn test_merge_ranges_single_invalid() {
-        let result = merge_and_order_ranges_vec(vec!['z'..='a']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['z'..='a']);
         assert_eq!(result, vec![]);
     }
 
     #[test]
     fn test_merge_ranges_multi_invalid() {
-        let result = merge_and_order_ranges_vec(vec!['z'..='a', 'Z'..='A', '9'..='0']);
+        let result: Vec<RangeInclusive<char>> =
+            merge_and_order_ranges(['z'..='a', 'Z'..='A', '9'..='0']);
         assert_eq!(result, vec![]);
     }
 
     #[test]
     fn test_merge_ranges_one_invalid1() {
-        let result = merge_and_order_ranges_vec(vec!['9'..='0', 'A'..='Z', 'a'..='z']);
+        let result: Vec<RangeInclusive<char>> =
+            merge_and_order_ranges(['9'..='0', 'A'..='Z', 'a'..='z']);
         assert_eq!(result, vec!['A'..='Z', 'a'..='z']);
     }
 
     #[test]
     fn test_merge_ranges_one_invalid2() {
-        let result = merge_and_order_ranges_vec(vec!['0'..='9', 'Z'..='A', 'a'..='z']);
+        let result: Vec<RangeInclusive<char>> =
+            merge_and_order_ranges(['0'..='9', 'Z'..='A', 'a'..='z']);
         assert_eq!(result, vec!['0'..='9', 'a'..='z']);
     }
 
     #[test]
     fn test_merge_ranges_one_invalid3() {
-        let result = merge_and_order_ranges_vec(vec!['0'..='9', 'A'..='Z', 'z'..='a']);
+        let result: Vec<RangeInclusive<char>> =
+            merge_and_order_ranges(['0'..='9', 'A'..='Z', 'z'..='a']);
         assert_eq!(result, vec!['0'..='9', 'A'..='Z']);
     }
 
     #[test]
     fn test_merge_ranges_single_overlap() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='a', 'Z'..='z']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='a', 'Z'..='z']);
         assert_eq!(result, vec!['A'..='z']);
     }
 
     #[test]
     fn test_merge_ranges_under_overlap() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='a', 'Z'..='z', 'B'..='b']);
+        let result: Vec<RangeInclusive<char>> =
+            merge_and_order_ranges(['A'..='a', 'Z'..='z', 'B'..='b']);
         assert_eq!(result, vec!['A'..='z']);
     }
 
     #[test]
     fn test_merge_ranges_multi_overlap() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='D', 'B'..='E', 'a'..='d', 'b'..='e']);
+        let result: Vec<RangeInclusive<char>> =
+            merge_and_order_ranges(['A'..='D', 'B'..='E', 'a'..='d', 'b'..='e']);
         assert_eq!(result, vec!['A'..='E', 'a'..='e']);
     }
 
     #[test]
     fn test_merge_ranges_overlap_start() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='C', 'A'..='E']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='C', 'A'..='E']);
         assert_eq!(result, vec!['A'..='E']);
     }
 
     #[test]
     fn test_merge_ranges_overlap_end() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='C', 'C'..='E']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='C', 'C'..='E']);
         assert_eq!(result, vec!['A'..='E']);
     }
 
     #[test]
     fn test_merge_ranges_overlap_end2() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='C', 'C'..='E', 'E'..='G']);
+        let result: Vec<RangeInclusive<char>> =
+            merge_and_order_ranges(['A'..='C', 'C'..='E', 'E'..='G']);
         assert_eq!(result, vec!['A'..='G']);
     }
 
     #[test]
     fn test_merge_ranges_allongside() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='C', 'D'..='E']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='C', 'D'..='E']);
         assert_eq!(result, vec!['A'..='E']);
     }
 
     #[test]
     fn test_merge_ranges_multi_allongside() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='C', 'D'..='E', 'F'..='H']);
+        let result: Vec<RangeInclusive<char>> =
+            merge_and_order_ranges(['A'..='C', 'D'..='E', 'F'..='H']);
         assert_eq!(result, vec!['A'..='H']);
     }
 
     #[test]
     fn test_merge_ranges_not_allongside() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='B', 'D'..='E']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='B', 'D'..='E']);
         assert_eq!(result, vec!['A'..='B', 'D'..='E']);
     }
 
     #[test]
     fn test_merge_ranges_char() {
-        let result = merge_and_order_ranges_vec(vec!['1'..='1']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['1'..='1']);
         assert_eq!(result, vec!['1'..='1']);
     }
 
     #[test]
     fn test_merge_ranges_chars() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='A', 'a'..='a']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='A', 'a'..='a']);
         assert_eq!(result, vec!['A'..='A', 'a'..='a']);
     }
 
     #[test]
     fn test_merge_ranges_chars_allongside() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='A', 'B'..='B']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='A', 'B'..='B']);
         assert_eq!(result, vec!['A'..='B']);
     }
 
     #[test]
     fn test_merge_ranges_chars_overlap() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='A', 'A'..='A']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='A', 'A'..='A']);
         assert_eq!(result, vec!['A'..='A']);
     }
 
     #[test]
     fn test_merge_ranges_range_char_overlap() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='D', 'D'..='D']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='D', 'D'..='D']);
         assert_eq!(result, vec!['A'..='D']);
     }
 
     #[test]
     fn test_merge_ranges_range_char_allongside() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='D', 'E'..='E']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='D', 'E'..='E']);
         assert_eq!(result, vec!['A'..='E']);
     }
 
     #[test]
     fn test_merge_ranges_char_range_overlap() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='A', 'A'..='D']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='A', 'A'..='D']);
         assert_eq!(result, vec!['A'..='D']);
     }
 
     #[test]
     fn test_merge_ranges_char_range_allongside() {
-        let result = merge_and_order_ranges_vec(vec!['A'..='A', 'B'..='E']);
+        let result: Vec<RangeInclusive<char>> = merge_and_order_ranges(['A'..='A', 'B'..='E']);
         assert_eq!(result, vec!['A'..='E']);
     }
 
     #[test]
-    fn test_flatten_range_lowercase_letters() {
-        let result = range_flatten_vec(vec![range_lowercase_letters()]).collect_vec();
+    fn test_flatten_RANGE_LOWERCASE_LETTERS() {
+        let result: Vec<char> = ranges_flatten([RANGE_LOWERCASE_LETTERS]);
         assert_eq!(
             result,
             vec![
@@ -278,8 +269,8 @@ mod tests {
     }
 
     #[test]
-    fn test_flatten_range_uppercase_letters() {
-        let result = range_flatten_vec(vec![range_uppercase_letters()]).collect_vec();
+    fn test_flatten_RANGE_UPPERCASE_LETTERS() {
+        let result: Vec<char> = ranges_flatten([RANGE_UPPERCASE_LETTERS]);
         assert_eq!(
             result,
             vec![
@@ -290,8 +281,8 @@ mod tests {
     }
 
     #[test]
-    fn test_flatten_range_letters() {
-        let result = range_flatten_vec(range_letters()).collect_vec();
+    fn test_flatten_RANGE_LETTERS() {
+        let result: Vec<char> = ranges_flatten(RANGE_LETTERS);
         assert_eq!(
             result,
             vec![
@@ -304,8 +295,8 @@ mod tests {
     }
 
     #[test]
-    fn test_flatten_range_numbers() {
-        let result = range_flatten_vec(vec![range_numbers()]).collect_vec();
+    fn test_flatten_RANGE_NUMBERS() {
+        let result: Vec<char> = ranges_flatten([RANGE_NUMBERS]);
         assert_eq!(
             result,
             vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -313,8 +304,8 @@ mod tests {
     }
 
     #[test]
-    fn test_flatten_range_hex_lowercase() {
-        let result = range_flatten_vec(range_hex_lowercase()).collect_vec();
+    fn test_flatten_RANGE_HEX_LOWERCASE() {
+        let result: Vec<char> = ranges_flatten(RANGE_HEX_LOWERCASE);
         assert_eq!(
             result,
             vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
@@ -322,8 +313,8 @@ mod tests {
     }
 
     #[test]
-    fn test_flatten_range_hex_uppercase() {
-        let result = range_flatten_vec(range_hex_uppercase()).collect_vec();
+    fn test_flatten_RANGE_HEX_UPPERCASE() {
+        let result: Vec<char> = ranges_flatten(RANGE_HEX_UPPERCASE);
         assert_eq!(
             result,
             vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
@@ -331,8 +322,8 @@ mod tests {
     }
 
     #[test]
-    fn test_flatten_range_hex() {
-        let result = range_flatten_vec(range_hex()).collect_vec();
+    fn test_flatten_RANGE_HEX() {
+        let result: Vec<char> = ranges_flatten(RANGE_HEX);
         assert_eq!(
             result,
             vec![
@@ -343,8 +334,8 @@ mod tests {
     }
 
     #[test]
-    fn test_flatten_range_alphanum_lowercase() {
-        let result = range_flatten_vec(range_alphanum_lowercase()).collect_vec();
+    fn test_flatten_RANGE_ALPHANUM_LOWERCASE() {
+        let result: Vec<char> = ranges_flatten(RANGE_ALPHANUM_LOWERCASE);
         assert_eq!(
             result,
             vec![
@@ -356,8 +347,8 @@ mod tests {
     }
 
     #[test]
-    fn test_flatten_range_alphanum_uppercase() {
-        let result = range_flatten_vec(range_alphanum_uppercase()).collect_vec();
+    fn test_flatten_RANGE_ALPHANUM_UPPERCASE() {
+        let result: Vec<char> = ranges_flatten(RANGE_ALPHANUM_UPPERCASE);
         assert_eq!(
             result,
             vec![
@@ -369,8 +360,8 @@ mod tests {
     }
 
     #[test]
-    fn test_flatten_range_alphanum() {
-        let result = range_flatten_vec(range_alphanum()).collect_vec();
+    fn test_flatten_RANGE_ALPHANUM() {
+        let result: Vec<char> = ranges_flatten(RANGE_ALPHANUM);
         assert_eq!(
             result,
             vec![
@@ -384,7 +375,7 @@ mod tests {
 
     #[test]
     fn test_flatten_range_punct() {
-        let result = range_flatten_vec(range_punct()).collect_vec();
+        let result: Vec<char> = ranges_flatten(RANGE_PUNCT);
         assert_eq!(
             result,
             vec![
@@ -394,6 +385,7 @@ mod tests {
         );
     }
 
+    #[test]
     fn test_range_macro() {
         let result = range!("abcABCdef321980");
         assert_eq!(result, vec!['0'..='3', '8'..='9', 'A'..='C', 'a'..='f'])
